0ec90d5339675a53c3c1d766c148ba4dc7061c9f
bsegers <briansegs@gmail.com>
2025-03-01 11:35:39 -0500

    Add logo and icon to cms

---
+++ src/components/Graphics/Icon.tsx
@@ -0,0 +1,10 @@
+export const Icon = () => {
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src="/favicon.svg"
+      alt="Manza Search Icon"
+      className="size-6 dark:rounded-lg dark:bg-white"
+    />
+  )
+}
---
+++ src/components/Graphics/Logo.tsx
@@ -0,0 +1,6 @@
+export const Logo = () => {
+  return (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img src="/favicon.svg" alt="Manza Search logo" className="size-64" />
+  )
+}
