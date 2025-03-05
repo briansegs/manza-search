a52b87e7212b9fc7284b40afc181c82d84bb0d9f
bsegers <briansegs@gmail.com>
2025-03-05 14:51:29 -0500

    Pass data to article and articles pages

--- src/app/(frontend)/articles/[slug]/page.tsx
+++ src/app/(frontend)/articles/[slug]/page.tsx
@@ -1,11 +1,85 @@
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
 import PageClient from './page.client'
+import { draftMode } from 'next/headers'
+import { PayloadRedirects } from '@/components/PayloadRedirects'
+import React, { cache } from 'react'
+import { generateMeta } from '@/utilities/generateMeta'
+import { Metadata } from 'next'
+import { LivePreviewListener } from '@/components/LivePreviewListener'
 
-export default async function Article() {
+export async function generateStaticParams() {
+  const payload = await getPayload({ config: configPromise })
+  const articles = await payload.find({
+    collection: 'articles',
+    draft: false,
+    limit: 1000,
+    overrideAccess: false,
+    pagination: false,
+    select: {
+      slug: true,
+    },
+  })
+
+  const params = articles.docs.map(({ slug }) => {
+    return { slug }
+  })
+
+  return params
+}
+
+type Args = {
+  params: Promise<{
+    slug?: string
+  }>
+}
+
+export default async function Article({ params: paramsPromise }: Args) {
+  const { isEnabled: draft } = await draftMode()
+  const { slug = '' } = await paramsPromise
+  const url = '/articles/' + slug
+  const article = await queryPostBySlug({ slug })
+
+  if (!article) return <PayloadRedirects url={url} />
+
   return (
     <article className="pb-16 pt-16">
       <PageClient />
 
-      <div>Article</div>
+      {/* Allows redirects for valid pages too */}
+      <PayloadRedirects disableNotFound url={url} />
+
+      {draft && <LivePreviewListener />}
+
+      <div className="container">{article.title}</div>
     </article>
   )
+}
+
+const queryPostBySlug = cache(async ({ slug }: { slug: string }) => {
+  const { isEnabled: draft } = await draftMode()
+
+  const payload = await getPayload({ config: configPromise })
+
+  const result = await payload.find({
+    collection: 'articles',
+    draft,
+    limit: 1,
+    overrideAccess: draft,
+    pagination: false,
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  })
+
+  return result.docs?.[0] || null
+})
+
+export async function generateMetadata({ params: paramsPromise }: Args): Promise<Metadata> {
+  const { slug = '' } = await paramsPromise
+  const article = await queryPostBySlug({ slug })
+
+  return generateMeta({ doc: article })
 }
--- src/app/(frontend)/articles/page.tsx
+++ src/app/(frontend)/articles/page.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
 import PageClient from './page.client'
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import Link from 'next/link'
+import { Metadata } from 'next'
+
+export const dynamic = 'force-static'
+export const revalidate = 600
 
 export default async function Page() {
+  const payload = await getPayload({ config: configPromise })
+
+  const articles = await payload.find({
+    collection: 'articles',
+    sort: 'createdAt',
+    depth: 1,
+    limit: 12,
+    overrideAccess: false,
+    select: {
+      title: true,
+      slug: true,
+      categories: true,
+      meta: true,
+    },
+  })
+
   return (
     <div className="pb-24 pt-24">
       <PageClient />
@@ -12,14 +35,24 @@ export default async function Page() {
       </div>
 
       <div className="container">
-        <ol className="flex flex-col gap-4">
-          <li>Article 1</li>
-          <li>Article 2</li>
-          <li>Article 3</li>
-          <li>Article 4</li>
-          <li>Article 5</li>
-        </ol>
+        {articles.totalDocs > 1 && articles.page && (
+          <ol className="flex flex-col gap-4">
+            {articles.docs.map(({ slug, meta }) => (
+              <li className="list-inside list-disc hover:underline" key={slug}>
+                <Link href={`/articles/${slug}`}>
+                  <span className="text-lg font-bold">{meta?.title}:</span> {meta?.description}
+                </Link>
+              </li>
+            ))}
+          </ol>
+        )}
       </div>
     </div>
   )
+}
+
+export function generateMetadata(): Metadata {
+  return {
+    title: `Articles | Manza Search`,
+  }
 }
