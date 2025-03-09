81638336152c61b8e8f97085bf5ea2bcb5ea5ad2
bsegers <briansegs@gmail.com>
2025-03-09 13:41:32 -0400

    Refactor and fix data checks and rendering

--- src/blocks/article-blocks/AdSection/Component.tsx
+++ src/blocks/article-blocks/AdSection/Component.tsx
@@ -24,20 +24,28 @@ export const AdSectionBlock: React.FC<AdSectionProps> = (props) => {
 
       <div className="border-content flex w-full justify-around gap-6 px-16 py-12">
         {ads && ads.length > 0 ? (
-          ads.map(({ id, link, media }) => (
-            <div
-              key={id}
-              className="border-content relative h-64 w-96 overflow-hidden rounded-lg hover:border-menu-red"
-            >
-              {link ? (
-                <CMSLink {...link}>{media ? renderMedia(media) : renderPlaceholder()}</CMSLink>
-              ) : media ? (
-                renderMedia(media)
-              ) : (
-                renderPlaceholder()
-              )}
-            </div>
-          ))
+          ads.map(({ id, link, media }) => {
+            const hasValidLink =
+              link && (link.type === 'reference' ? link.reference : link.type === 'custom')
+            const hasValidMedia = media && media.url
+
+            return (
+              <div
+                key={id}
+                className="border-content relative h-64 w-96 overflow-hidden rounded-lg hover:border-menu-red"
+              >
+                {hasValidLink ? (
+                  <CMSLink {...link}>
+                    {hasValidMedia ? renderMedia(media) : renderPlaceholder()}
+                  </CMSLink>
+                ) : hasValidMedia ? (
+                  renderMedia(media)
+                ) : (
+                  renderPlaceholder()
+                )}
+              </div>
+            )
+          })
         ) : (
           <div className="py-4 text-center">No advertisements available</div>
         )}
