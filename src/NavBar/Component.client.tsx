2c3fa574a8a10740f051bdf782f95d997110dd54
bsegers <briansegs@gmail.com>
2025-03-14 11:58:09 -0400

    Made article page responsive

--- src/NavBar/Component.client.tsx
+++ src/NavBar/Component.client.tsx
@@ -13,15 +13,15 @@ export const NavBarClient: React.FC<NavBarClientProps> = ({ data }) => {
 
   return (
     <div className="flex h-16 bg-black">
-      <div className="container flex w-2/3 items-center justify-between rounded-b-xl border-4 border-black bg-navBar">
-        <nav className="flex w-full items-center justify-center gap-8">
+      <div className="container flex w-2/3 items-center justify-between overflow-x-auto rounded-b-xl border-4 border-black bg-navBar">
+        <nav className="container flex w-full items-center justify-between gap-2">
           {navItems.map(({ link }, i) => {
             return (
               <CMSLink
                 key={i}
                 {...link}
                 appearance="link"
-                className="font-serif text-xl uppercase text-white hover:text-blue-800 hover:no-underline"
+                className="font-serif text-lg uppercase text-white hover:text-blue-800 hover:no-underline xl:text-xl"
               />
             )
           })}
