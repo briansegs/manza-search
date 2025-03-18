234cf8c4c7979fa8d0d80a2e3e8bfe28775d0173
bsegers <briansegs@gmail.com>
2025-03-18 12:16:13 -0400

    Add artiles to sitemap

--- src/app/(frontend)/(sitemaps)/articles-sitemap.xml/route.ts
+++ src/app/(frontend)/(sitemaps)/articles-sitemap.xml/route.ts
@@ -3,7 +3,7 @@ import { getPayload } from 'payload'
 import config from '@payload-config'
 import { unstable_cache } from 'next/cache'
 
-const getPagesSitemap = unstable_cache(
+const getArticlesSitemap = unstable_cache(
   async () => {
     const payload = await getPayload({ config })
     const SITE_URL =
@@ -12,7 +12,7 @@ const getPagesSitemap = unstable_cache(
       'https://example.com'
 
     const results = await payload.find({
-      collection: 'pages',
+      collection: 'articles',
       overrideAccess: false,
       draft: false,
       depth: 0,
@@ -31,42 +31,25 @@ const getPagesSitemap = unstable_cache(
 
     const dateFallback = new Date().toISOString()
 
-    const defaultSitemap = [
-      {
-        loc: `${SITE_URL}/search`,
-        lastmod: dateFallback,
-      },
-      {
-        loc: `${SITE_URL}/posts`,
-        lastmod: dateFallback,
-      },
-      {
-        loc: `${SITE_URL}/articles`,
-        lastmod: dateFallback,
-      },
-    ]
-
     const sitemap = results.docs
       ? results.docs
-          .filter((page) => Boolean(page?.slug))
-          .map((page) => {
-            return {
-              loc: page?.slug === 'home' ? `${SITE_URL}/` : `${SITE_URL}/${page?.slug}`,
-              lastmod: page.updatedAt || dateFallback,
-            }
-          })
+          .filter((article) => Boolean(article?.slug))
+          .map((article) => ({
+            loc: `${SITE_URL}/articles/${article?.slug}`,
+            lastmod: article.updatedAt || dateFallback,
+          }))
       : []
 
-    return [...defaultSitemap, ...sitemap]
+    return sitemap
   },
-  ['pages-sitemap'],
+  ['articles-sitemap'],
   {
-    tags: ['pages-sitemap'],
+    tags: ['articles-sitemap'],
   },
 )
 
 export async function GET() {
-  const sitemap = await getPagesSitemap()
+  const sitemap = await getArticlesSitemap()
 
   return getServerSideSitemap(sitemap)
 }
