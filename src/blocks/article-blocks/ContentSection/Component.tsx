5a70150300b9741824ea478d1eee8a87390ebed2
bsegers <briansegs@gmail.com>
2025-03-22 12:32:42 -0400

    Make content font sarif

--- src/blocks/article-blocks/ContentSection/Component.tsx
+++ src/blocks/article-blocks/ContentSection/Component.tsx
@@ -2,10 +2,9 @@ import { ContentSection as ContentSectionProps, Media } from '@/payload-types'
 import React from 'react'
 import { renderMedia, TitleBar } from '../components'
 import RichText from '@/components/RichText'
-import { Button } from '@/components/ui/button'
-import clsx from 'clsx'
 
-const buttonStyles = 'bg-slate-200 hover:bg-slate-300 text-black border-2 border-black'
+import clsx from 'clsx'
+import ContentButtons from './ContentButtons'
 
 const ImageContainer = ({ image }: { image: Media }) => {
   if (!image) return null
@@ -28,11 +27,7 @@ export const ContentSection: React.FC<ContentSectionProps> = (props) => {
 
       <div className="border-content flex w-full flex-col gap-4 px-4 py-6 sm:gap-8 sm:px-16 sm:py-12">
         <div className="flex justify-end">
-          <div className="flex w-64 justify-around">
-            <Button className={`${buttonStyles}`}>B</Button>
-            <Button className={`${buttonStyles}`}>A</Button>
-            <Button className={`${buttonStyles}`}>LM</Button>
-          </div>
+          <ContentButtons />
         </div>
 
         {alignment !== 'left & right' ? (
@@ -46,7 +41,7 @@ export const ContentSection: React.FC<ContentSectionProps> = (props) => {
 
             {content && (
               <div className={clsx(image1 && 'mt-2')}>
-                <RichText data={content} enableGutter={false} />
+                <RichText className="font-serif" data={content} enableGutter={false} />
               </div>
             )}
           </div>
