b3a085eee987c6ae843e6fef5e9665f9856f4dd1
bsegers <briansegs@gmail.com>
2025-03-25 16:45:17 -0400

    Remove theme selector

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -6,7 +6,6 @@ import type { Footer } from '@/payload-types'
 
 import { CMSLink } from '@/components/Link'
 import { Logo } from '@/components/Logo/Logo'
-import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
 
 export async function Footer() {
   const footerData: Footer = await getCachedGlobal('footer', 1)()
@@ -22,8 +21,6 @@ export async function Footer() {
         </Link>
 
         <div className="flex flex-col-reverse items-start gap-4 md:flex-row md:items-center">
-          <ThemeSelector />
-
           <nav className="flex flex-col gap-4 md:flex-row">
             {navItems.map(({ link }, i) => {
               return <CMSLink className="text-white" key={i} {...link} />
