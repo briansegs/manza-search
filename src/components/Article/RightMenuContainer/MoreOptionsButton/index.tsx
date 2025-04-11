37f206bc535242302d99c7e354aa7927479738ed
bsegers <briansegs@gmail.com>
2025-04-11 16:19:07 -0400

    Fix file name and typos

--- src/components/Article/RightMenuContainer/MoreOptionsButton/index.tsx
+++ src/components/Article/RightMenuContainer/MoreOptionsButton/index.tsx
@@ -6,11 +6,11 @@ import React from 'react'
 const buttonStyles =
   'size-14 rounded-full border-4 border-black font-serif text-white shadow-[10px_10px_10px_black] hover:text-white/50'
 
-interface MOMuneButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface MOMenuButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string
 }
 
-const MOMuneButton: React.FC<MOMuneButtonProps> = ({ className, ...props }) => (
+const MOMenuButton: React.FC<MOMenuButtonProps> = ({ className, ...props }) => (
   <button className={cn(buttonStyles, className)} {...props} />
 )
 
@@ -22,24 +22,25 @@ const MoreOptions: React.FC = () => {
           MO
         </Button>
       </PopoverTrigger>
+
       <PopoverContent
         side={'left'}
         sideOffset={80}
         className="mt-12 flex h-64 w-fit gap-2 border-0 bg-transparent p-0 shadow-none sm:mt-0"
       >
         <div className="flex w-[90px] flex-col justify-between gap-2 py-9">
-          <MOMuneButton className="self-end bg-[#0011ff]">P</MOMuneButton>
-          <MOMuneButton className="bg-[#ff1c00]">M</MOMuneButton>
-          <MOMuneButton className="self-end bg-[#8a1384]">E</MOMuneButton>
+          <MOMenuButton className="self-end bg-[#0011ff]">P</MOMenuButton>
+          <MOMenuButton className="bg-[#ff1c00]">M</MOMenuButton>
+          <MOMenuButton className="self-end bg-[#8a1384]">E</MOMenuButton>
         </div>
 
         <div className="flex flex-col justify-between py-1.5">
-          <MOMuneButton className="bg-[#ffc8ce]">S</MOMuneButton>
-          <MOMuneButton className="bg-[#006c00]">K</MOMuneButton>
+          <MOMenuButton className="bg-[#ffc8ce]">S</MOMenuButton>
+          <MOMenuButton className="bg-[#006c00]">K</MOMenuButton>
         </div>
 
         <div className="h-60">
-          <MOMuneButton className="ml-1 bg-[#0011ff]">B</MOMuneButton>
+          <MOMenuButton className="ml-1 bg-[#0011ff]">B</MOMenuButton>
         </div>
       </PopoverContent>
     </Popover>
