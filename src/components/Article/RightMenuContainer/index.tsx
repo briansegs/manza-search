a00a671b2fa508dc009dd98fc09024a11af3e8d8
bsegers <briansegs@gmail.com>
2025-04-11 16:06:15 -0400

    Fix file and folder name

--- src/components/Article/RightMenuContainer/index.tsx
+++ src/components/Article/RightMenuContainer/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import DictionaryButton from './DictionaryButton'
-import MoreOptions from './MoreOptions.tsx'
+import MoreOptions from './MoreOptions'
 
 export const RightMenu = () => (
   <div className="relative flex size-fit flex-col items-center justify-center rounded-xl bg-menu-red p-1 shadow-[10px_10px_10px_black] hover:bg-black sm:size-28 sm:flex-row sm:p-0">
