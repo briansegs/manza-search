b5e9848d1342c977321ee090892311196a0f8477
bsegers <briansegs@gmail.com>
2025-05-27 10:07:24 -0400

    Change dictionary button alignment on mobile pages

--- src/components/HealthAndWellness/TopMenuContainer.tsx
+++ src/components/HealthAndWellness/TopMenuContainer.tsx
@@ -5,7 +5,7 @@ const TopMenuContainer: React.FC = () => {
   return (
     <div className="sticky top-0 z-10 mt-1">
       {/* Only visible on mobile devices */}
-      <div className="ml-auto flex px-1 sm:hidden lg:hidden">
+      <div className="ml-auto flex justify-end px-1 sm:hidden lg:hidden">
         <RightMenu />
       </div>
     </div>
