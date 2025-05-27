e123ee1c69cfaacfec5a71f528101306bad5a43a
bsegers <briansegs@gmail.com>
2025-05-27 09:49:55 -0400

    Turned dictionary butt horizantal on mobile

--- src/components/RightMenuContainer/index.tsx
+++ src/components/RightMenuContainer/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import DictionaryButton from './DictionaryButton'
 import MoreOptionsButton from './MoreOptionsButton'
+import { cn } from '@/utilities/ui'
 
 export const RightMenu = () => (
-  <div className="relative flex size-fit flex-col items-center justify-center rounded-xl bg-menu-red p-1 shadow-[10px_10px_10px_black] hover:bg-black sm:size-28 sm:flex-row sm:p-0">
+  <div
+    className={cn(
+      'relative flex size-fit items-center justify-center rounded-xl bg-menu-red px-2 shadow-[10px_10px_10px_black] hover:bg-black',
+      'sm:size-28 sm:p-0',
+    )}
+  >
     <DictionaryButton />
 
     <MoreOptionsButton />
