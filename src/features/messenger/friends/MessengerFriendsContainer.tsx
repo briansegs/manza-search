bd68a4251ec5cb7a21840771e8479e0b2c7b9d58
bsegers <briansegs@gmail.com>
2025-07-07 09:44:13 -0400

    Adjust friends placeholder and loading placeholder

--- src/features/messenger/friends/MessengerFriendsContainer.tsx
+++ src/features/messenger/friends/MessengerFriendsContainer.tsx
@@ -21,10 +21,14 @@ export function MessengerFriendsContainer({ activeConversation }: MessengerFrien
       title="Friends"
       action={<AddFriendDialog />}
     >
-      {!requests && <Loader2 className="h-8 w-8" />}
+      {!requests && (
+        <div className="flex h-[80%] w-full items-center justify-center">
+          <Loader2 className="h-8 w-8 animate-spin" />
+        </div>
+      )}
 
       {requests?.length === 0 && (
-        <p className="flex h-full w-full items-center justify-center">No friend requests found</p>
+        <p className="flex h-[80%] w-full items-center justify-center">No friend requests found</p>
       )}
 
       {requests?.map((request) => (
