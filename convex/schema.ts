adbca5d27eda67c972ebc68d06d170812b40e573
bsegers <briansegs@gmail.com>
2025-07-07 10:09:01 -0400

    fix: Incorrect table reference in messages schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -40,7 +40,7 @@ export default defineSchema({
     .index('by_memberId_conversationId', ['memberId', 'conversationId']),
   messages: defineTable({
     senderId: v.id('users'),
-    conversationId: v.id('conversationId'),
+    conversationId: v.id('conversations'),
     type: v.string(),
     content: v.array(v.string()),
   }).index('by_conversationId', ['conversationId']),
