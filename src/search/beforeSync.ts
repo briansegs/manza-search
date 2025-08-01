660d1a768465dd7ddae568947a81db04a26906a9
bsegers <briansegs@gmail.com>
2025-07-31 20:48:50 -0400

    Attach category data to payload

--- src/search/beforeSync.ts
+++ src/search/beforeSync.ts
@@ -1,6 +1,6 @@
 import { BeforeSync, DocToSync } from '@payloadcms/plugin-search/types'
 
-export const beforeSyncWithSearch: BeforeSync = async ({ originalDoc, searchDoc }) => {
+export const beforeSyncWithSearch: BeforeSync = async ({ originalDoc, searchDoc, payload }) => {
   const {
     doc: { relationTo: collection },
   } = searchDoc
@@ -22,17 +22,23 @@ export const beforeSyncWithSearch: BeforeSync = async ({ originalDoc, searchDoc 
   if (categories && Array.isArray(categories) && categories.length > 0) {
     // get full categories and keep a flattened copy of their most important properties
     try {
-      const mappedCategories = categories.map((category) => {
-        const { id, title } = category
+      const populated = await Promise.all(
+        categories.map(async (cat) => {
+          // cat could be an id string or an object with an id
+          const categoryId = typeof cat === 'string' ? cat : cat.id
+          const category = await payload.findByID({
+            collection: 'categories',
+            id: categoryId,
+          })
 
-        return {
-          relationTo: 'categories',
-          id,
-          title,
-        }
-      })
-
-      modifiedDoc.categories = mappedCategories
+          return {
+            relationTo: 'categories',
+            id: category.id,
+            title: category.title,
+          }
+        }),
+      )
+      modifiedDoc.categories = populated
     } catch (_err) {
       console.error(
         `Failed. Category not found when syncing collection '${collection}' with id: '${id}' to search.`,
