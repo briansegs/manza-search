31e54e4912fbfbc526ae021dd6fc23226e3f8388
bsegers <briansegs@gmail.com>
2025-08-15 18:38:18 -0400

    Type compatibility — Make RenderMedia accept AdMedia

--- src/features/shared/components/RenderMedia.tsx
+++ src/features/shared/components/RenderMedia.tsx
@@ -1,8 +1,8 @@
 import { Media } from '@/components/Media'
-import { Media as MediaType } from '@/payload-types'
+import { Media as MediaType, AdMedia } from '@/payload-types'
 
 import React from 'react'
 
-export function RenderMedia({ media }: { media: string | MediaType }) {
+export function RenderMedia({ media }: { media: string | MediaType | AdMedia }) {
   return <Media resource={media} imgClassName="size-full object-cover" fill />
 }
