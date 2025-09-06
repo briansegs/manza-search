6d9340aad3752a46dea7cb0cac08041c397c9404
bsegers <briansegs@gmail.com>
2025-09-06 15:35:44 -0400

    Make link url absolute

--- src/features/articles/components/ImagesSection/ArticleImagesLink.tsx
+++ src/features/articles/components/ImagesSection/ArticleImagesLink.tsx
@@ -5,7 +5,7 @@ import { ArticleImagesLinkProps } from './types'
 export function ArticleImagesLink({ slug }: ArticleImagesLinkProps) {
   return (
     <Button className="text-blue-500" variant="link" asChild>
-      <Link href={`${slug}/images`}>View More</Link>
+      <Link href={`/articles/${slug}/images`}>View More</Link>
     </Button>
   )
 }
