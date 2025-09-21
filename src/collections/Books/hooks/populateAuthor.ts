eaa36bb50bd5e7746d88c7bc55c6a1e69643f808
bsegers <briansegs@gmail.com>
2025-09-21 18:55:40 -0400

    fix: Typo in comment

--- src/collections/Books/hooks/populateAuthor.ts
+++ src/collections/Books/hooks/populateAuthor.ts
@@ -2,9 +2,9 @@ import type { CollectionAfterReadHook } from 'payload'
 import { User } from 'src/payload-types'
 
 // The `user` collection has access control locked so that users are not publicly accessible
-// This means that we need to populate the authors manually here to protect user privacy
+// This means that we need to populate the author manually here to protect user privacy
 // GraphQL will not return mutated user data that differs from the underlying schema
-// So we use an alternative `populatedAuthors` field to populate the user data, hidden from the admin UI
+// So we use an alternative `populatedAuthor` field to populate the user data, hidden from the admin UI
 export const populateAuthor: CollectionAfterReadHook = async ({ doc, req, req: { payload } }) => {
   if (!doc?.content) return doc
   const rawAuthor = doc.content.author
