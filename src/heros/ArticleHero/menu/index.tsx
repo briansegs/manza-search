8227c1c113a467933ff4d130de221cd66529edd7
bsegers <briansegs@gmail.com>
2025-11-01 17:58:59 -0400

    feat: Render different menus based on auth state

--- src/heros/ArticleHero/menu/index.tsx
+++ src/heros/ArticleHero/menu/index.tsx
@@ -1,57 +1,15 @@
 'use client'
 
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 import { SectionOptions, useFilo } from '@/providers/FiloProvider'
-import { cn } from '@/utilities/ui'
 import { Authenticated, Unauthenticated, AuthLoading } from 'convex/react'
-import { AlertCircle } from 'lucide-react'
 
-type FiloUnauthenticatedPopoverProps = {
-  triggerTitle: string
-}
-
-export function FiloUnauthenticatedPopover({ triggerTitle }: FiloUnauthenticatedPopoverProps) {
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <button className="font-serif text-xl hover:text-secondary-blue focus:outline-none">
-          {triggerTitle}
-        </button>
-      </PopoverTrigger>
-
-      <PopoverContent className="min-w-fit max-w-md border-black bg-menu text-white" asChild>
-        <Alert variant="destructive">
-          <AlertCircle />
-          <AlertTitle className="mt-1 text-red-400">Unable to initialize Filo menu</AlertTitle>
-
-          <AlertDescription>Sign in to use the Filo menu</AlertDescription>
-        </Alert>
-      </PopoverContent>
-    </Popover>
-  )
-}
-
-export function MenuUnauthenticated() {
-  return (
-    <div className="flex items-center gap-6">
-      <FiloUnauthenticatedPopover triggerTitle="Menu" />
-
-      <ul className="list-inside list-disc space-y-1 font-serif text-lg">
-        {filoSections.map((section) => (
-          <li key={section}>
-            <FiloUnauthenticatedPopover triggerTitle={section} />
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
-const filoSections = ['pin', 'save', 'history', 'lists'] as const
+import { FiloMenu } from './FiloMenu'
+import { FiloMenuUnauthenticated } from './FiloMenuUnauthenticated'
 
 export function ArticleHeroMenu() {
   const { setOpen, setSection } = useFilo()
+
+  const filoSections = ['pin', 'save', 'history', 'lists'] as const
 
   const handleOpen = (section: SectionOptions) => {
     setOpen(true)
@@ -61,40 +19,15 @@ export function ArticleHeroMenu() {
   return (
     <>
       <Authenticated>
-        <div className="flex items-center gap-6">
-          <button
-            type="button"
-            onClick={() => handleOpen('pin')}
-            className="font-serif text-xl hover:text-secondary-blue focus:outline-none"
-          >
-            Menu
-          </button>
-
-          <ul className="list-inside list-disc space-y-1 font-serif text-lg">
-            {filoSections.map((section) => (
-              <li key={section}>
-                <button
-                  type="button"
-                  onClick={() => handleOpen(section)}
-                  className={cn(
-                    'cursor-pointer hover:text-secondary-blue focus:outline-none',
-                    'capitalize',
-                  )}
-                >
-                  {section}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <FiloMenu handleOpen={handleOpen} filoSections={filoSections} />
       </Authenticated>
 
       <Unauthenticated>
-        <MenuUnauthenticated />
+        <FiloMenuUnauthenticated filoSections={filoSections} />
       </Unauthenticated>
 
       <AuthLoading>
-        <MenuUnauthenticated />
+        <FiloMenu disabled filoSections={filoSections} />
       </AuthLoading>
     </>
   )
