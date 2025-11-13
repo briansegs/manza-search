6623b525df4fad9c8004da02b2ac64a11f5b0f86
bsegers <briansegs@gmail.com>
2025-11-13 14:31:50 -0500

    feat: Added list to extra content popover

--- src/features/articles/components/ExtraContentPopoverMenu.tsx
+++ src/features/articles/components/ExtraContentPopoverMenu.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { PopoverContent } from '@/components/ui/popover'
+import { ArticleMedia, Book } from '@/payload-types'
+import { ListsButtonAuthenticated } from './ArticleTopMenu/Lists/ListsButtonAuthenticated'
 
 type MenuItem = {
   name: string
@@ -9,13 +11,35 @@ type MenuItem = {
 
 export type ExtraContentPopoverMenuProps = {
   menuItems: MenuItem[]
+  content: string | null | ArticleMedia | Book
+  isSignedIn?: boolean
+  contentType: 'article' | 'image' | 'book'
 }
 
-export function ExtraContentPopoverMenu({ menuItems }: ExtraContentPopoverMenuProps) {
+export function ExtraContentPopoverMenu({
+  menuItems,
+  content,
+  isSignedIn,
+  contentType,
+}: ExtraContentPopoverMenuProps) {
   return (
     <PopoverContent className="flex w-40 flex-col items-center border-2 border-black">
       <div className="font-serif text-lg font-bold text-secondary-blue">Extra Content</div>
       <ul className="flex w-full flex-col items-center">
+        {content && typeof content === 'object' && (
+          <li className="w-full">
+            <ListsButtonAuthenticated content={content} contentType={contentType}>
+              <Button
+                variant="ghost"
+                className="w-full font-serif font-bold"
+                disabled={!isSignedIn}
+              >
+                list
+              </Button>
+            </ListsButtonAuthenticated>
+          </li>
+        )}
+
         {menuItems.map(({ name, onClick, disabled }) => {
           return (
             <li key={name} className="w-full">
