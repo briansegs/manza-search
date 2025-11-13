7d9e5f5df3fa18b6e176d276ceeef2ee4bc39f08
bsegers <briansegs@gmail.com>
2025-11-13 14:22:57 -0500

    feat: Created list server actions

--- src/actions/schemas.ts
+++ src/actions/schemas.ts
@@ -5,10 +5,20 @@ type Saves = Doc<'savedContent'>
 
 type Pins = Doc<'pinnedContent'>
 
+type ListItems = {
+  _id: string
+  name: string
+  items: Doc<'listedContent'>[]
+}
+
 export const fetchSavedContentSchema = z.object({
   saveList: z.array(z.custom<Saves>()),
 })
 
 export const fetchPinnedContentSchema = z.object({
   pinList: z.array(z.custom<Pins>()),
+})
+
+export const fetchListedContentSchema = z.object({
+  listItems: z.array(z.custom<ListItems>()),
 })
